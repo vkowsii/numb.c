9f5d781f0a5e36eb844cf26968f06dda62cc73a6
vkowsii <30829409+vkowsii@users.noreply.github.com>
2018-02-09 14:28:06 +0530

    Create numb.c

---
+++ numb.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("enter the character");
+    scanf("%d",&ch);
+    if(ch<=a&&ch<=z)&&(ch<=A&&ch<=Z)
+    {
+        printf("character");
+    }
+    printf("not character");
+}
